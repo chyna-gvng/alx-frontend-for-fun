43b97dd4af37b926ed0ff7542692244494019a9c
Walter <w.angoye@alustudent.com>
2023-07-10 16:32:19 +0300

    Updated markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -25,17 +25,19 @@ with open(input_file, 'r') as file:
 html_content = re.sub(r'^# (.+)$', r'<h1>\g<1></h1>', markdown_content, flags=re.MULTILINE)
 
 # Parse unordered lists and generate HTML
-html_content = re.sub(r'(?<=\n)- (.+)', r'<li>\g<1></li>', html_content)
-html_content = re.sub(r'(?<=\n)<li>(.+)</li>(?=\n)', r'<ul>\n\g<0>\n</ul>', html_content)
+html_content = re.sub(r'^- (.+)$', r'<li>\g<1></li>', html_content, flags=re.MULTILINE)
+html_content = re.sub(r'<ul>\s*</p>', r'<ul>', html_content)
+html_content = re.sub(r'</p>\s*</ul>', r'</ul>', html_content)
 
 # Parse ordered lists and generate HTML
-html_content = re.sub(r'(?<=\n)\* (.+)', r'<li>\g<1></li>', html_content)
-html_content = re.sub(r'(?<=\n)<li>(.+)</li>(?=\n)', r'<ol>\n\g<0>\n</ol>', html_content)
+html_content = re.sub(r'^\* (.+)$', r'<li>\g<1></li>', html_content, flags=re.MULTILINE)
+html_content = re.sub(r'<ol>\s*</p>', r'<ol>', html_content)
+html_content = re.sub(r'</p>\s*</ol>', r'</ol>', html_content)
 
 # Parse paragraphs and generate HTML
-html_content = re.sub(r'(?<=\n)[^\n]+', r'<p>\g<0></p>', html_content)
-html_content = re.sub(r'(?<=\n)</(ul|ol)>', r'</\g<1>>\n', html_content)
-html_content = re.sub(r'(?<=\n)<(ul|ol)>', r'\n<\g<1>>', html_content)
+html_content = re.sub(r'^(.+)$', r'<p>\g<1></p>', html_content, flags=re.MULTILINE)
+html_content = re.sub(r'</(ul|ol)>\n<p>', r'</\g<1>>\n<p>', html_content)
+html_content = re.sub(r'</p>\n<(ul|ol)>', r'</p>\n</\g<1>>', html_content)
 
 with open(output_file, 'w') as file:
     file.write(html_content)
