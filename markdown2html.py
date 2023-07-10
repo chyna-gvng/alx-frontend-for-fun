d6376291ffa5df17fbd3fbcf6d1581162144ef75
Walter <w.angoye@alustudent.com>
2023-07-10 16:06:31 +0300

    Updated markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -5,6 +5,7 @@
 """
 import sys
 import os.path
+import re
 
 if len(sys.argv) < 3:
     sys.stderr.write("Usage: ./markdown2html.py <input_file> <output_file>\n")
@@ -17,7 +18,13 @@ if not os.path.exists(input_file):
     sys.stderr.write("Missing {}\n".format(input_file))
     sys.exit(1)
 
-# Perform the Markdown to HTML conversion here
-# ...
+with open(input_file, 'r') as file:
+    markdown_content = file.read()
+
+# Parse headings and generate HTML
+html_content = re.sub(r'^(#+)\s*(.+)$', r'<\g<1>>\g<2></\g<1>>', markdown_content, flags=re.MULTILINE)
+
+with open(output_file, 'w') as file:
+    file.write(html_content)
 
 sys.exit(0)
