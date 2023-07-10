062d1754dd51ca10e53a2d20dff3bb23bbd16c51
Walter <w.angoye@alustudent.com>
2023-07-10 16:00:26 +0300

    Updated markdown2html.py

---
+++ markdown2html.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    python script to convert markdown to html
+    usage: ./markdown2html.py <input_file> <output_file>
+"""
+import sys
+import os.path
+
+if len(sys.argv) < 3:
+    sys.stderr.write("Usage: ./markdown2html.py <input_file> <output_file>\n")
+    sys.exit(1)
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+if not os.path.exists(input_file):
+    sys.stderr.write("Missing {}\n".format(input_file))
+    sys.exit(1)
+
+# Perform the Markdown to HTML conversion here
+# ...
+
+sys.exit(0)
